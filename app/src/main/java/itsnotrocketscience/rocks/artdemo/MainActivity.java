6079552c0c0776bc6fe3e26fa8b8114a2ff7bcb2
lDuffy <liam.j.duffy5@gmail.com>
2015-07-10 00:53:51 +0100

    added button to open camera activiet

--- app/src/main/java/itsnotrocketscience/rocks/artdemo/MainActivity.java
+++ app/src/main/java/itsnotrocketscience/rocks/artdemo/MainActivity.java
@@ -4,15 +4,18 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnDemo = (Button)findViewById(R.id.btnDemo);
+        btnDemo.setOnClickListener(this);
     }
 
     @Override
@@ -35,5 +38,14 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.btnDemo:
+
+        }
+
     }
 }
